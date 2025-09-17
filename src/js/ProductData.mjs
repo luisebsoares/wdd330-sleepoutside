15f9f67013ef950f00c9a5d52ac3de8b420b6f61
LuisBrasil <lsoares@byupathway.edu>
2025-09-16 19:54:49 -0600

    final3

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -1,4 +1,4 @@
-const baseURL = import.meta.env.VITE_SERVER_URL;
+const baseURL = import.meta.env.VITE_SERVER_URL
 
 function convertToJson(res) {
   if (res.ok) {
@@ -12,9 +12,8 @@ export default class ProductData {
   constructor() {
   }
   async getData(category) {
-    const response = await fetch(`${baseURL}products/search/${category}`);
+    const response = await fetch(`${baseURL}products/search/${category} `);
     const data = await convertToJson(response);
-
     return data.Result;
   }
   async findProductById(id) {
